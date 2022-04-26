0b1a51b80c2cd2ef3c3a57990a015b26e0a27d90
Ilia Kutenkov <ilkute@utu.fi>
2022-04-26 11:56:48 +0300

    Added optimizer file and implemented Stochastic Gradient Descend in it

---
+++ thesis_code/optimizer.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class SGD:
+    def __init__(self, learning_rate):
+        self.lr = learning_rate
+
+    def forward(self, layer):
+        #Simply substract derivative of weight and bias multiplied by the learning rate
+        layer.weights = layer.weights - (self.lr * layer.weight_gradient)
+        print(layer.bias_gradient * self.lr)
+        layer.biases = layer.biases - (self.lr * layer.bias_gradient)
