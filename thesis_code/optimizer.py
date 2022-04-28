ca86fa62a985ebdea2bdce0237a5a42fd1d21450
ilikop009@gmail.com <ilkute@utu.fi>
2022-04-28 16:06:13 +0300

    Added learning rate decay

--- thesis_code/optimizer.py
+++ thesis_code/optimizer.py
@@ -1,11 +1,27 @@
 import numpy as np
 
 class SGD:
-    def __init__(self, learning_rate):
+    def __init__(self, model, learning_rate, decay = 0):
+        #Set the model that this obect is going to be working with
+        self.model = model
+        #Initialize starting learning rate
+        self.learning_rate = learning_rate
+        #Initialize actual learning rate
         self.lr = learning_rate
+        #Set the decay
+        self.decay = decay
+        #Initialize "step" counter for updating the learning rate 
+        self.step = 0
 
-    def forward(self, model):
-        for layer in model.layers:
-            #Simply substract derivative of weight and bias multiplied by the learning rate
-            layer.weights = layer.weights - (self.lr * layer.weight_gradient)
-            layer.biases = layer.biases - (self.lr * layer.bias_gradient)
+    def forward(self):
+        #Decay the current learning rate
+        #When decay is 0, it doesen't affect the learning rate at all
+        self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.step))
+        #Go through each layer in the model
+        for lay in self.model.layers:
+            #Simply substract gradients of weight and bias multiplied by the learning rate
+            lay.weights += (-self.current_learning_rate * lay.weight_gradient)
+            lay.biases += (-self.current_learning_rate * lay.bias_gradient)
+        #Update step counter
+        self.step += 1
+    
