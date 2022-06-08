53bfdbea6ccb923f2403544dc5116b73c2509410
ilikop <34922766+ilikop@users.noreply.github.com>
2022-06-08 11:20:27 +0300

    Beautified the code

--- thesis_code/main.py
+++ thesis_code/main.py
@@ -1,3 +1,4 @@
+from audioop import avg
 import layer
 import model
 import lossfun
@@ -9,14 +10,14 @@ import numpy as np
 
 
 
-def mnist():
+def mnist_train():
     #Initialize the model with crossenthropy loss
     mymodel = model.Model(lossfun.CrossEnthropy)
 
     #Create layers
-    dense1 = layer.Dense(784, 100, actfun.ReLu)
-    dense2 = layer.Dense(100, 50, actfun.ReLu)
-    dense3 = layer.Dense(50, 10, actfun.Softmax)
+    dense1 = layer.Dense(784, 15, actfun.ReLu)
+    dense2 = layer.Dense(15, 20, actfun.ReLu)
+    dense3 = layer.Dense(20, 10, actfun.Softmax)
 
     #Add layers to the model
     mymodel.addLayer(dense1)
@@ -41,25 +42,32 @@ def mnist():
     classes = []
 
     #Go through each image in mnist_data
-    for image in range(len(mnist_data) - 1):
+    for image in range(len(mnist_data)):
         #Append new class value(see the mnist dataset structure)
         classes.append(int(mnist_data[image][0]))
-        #We want to have neural network's inputs in the range of 0.01 - 0.99
-        mnist_data[image] = ((np.asfarray(mnist_data[image].split(",")[1:]) / 255.0 * 0.99) + 0.01)
+        #We want to have neural network's inputs in the range of 0.00001 - 0.99999
+        mnist_data[image] = ((np.asfarray(mnist_data[image].split(",")[1:]) / 255.0 * 0.99999) + 0.00001)
+        #print(mnist_data[image])
 
     #Set the batch size
-    test_batch = 50
+    batch_size = 50
     #Set the desired amount of epochs
-    epochs_amount = 30
+    epochs_amount = 10
 
     # Train in loop
     for epoch in range(epochs_amount):
 
+        accuracy_epoch = 0
         #This is a raw solution. I will change the last bit later on
-        for imagen in range(0,(len(mnist_data)-1) - test_batch,test_batch):
+        for imagen in range(0,(len(mnist_data)),batch_size):
             #Get the arrays of images and classes
-            batch = mnist_data[imagen:imagen+test_batch]
-            class_batch = classes[imagen:imagen+test_batch]
+            batch = mnist_data[imagen:imagen+batch_size]
+            class_batch = classes[imagen:imagen+batch_size]
+
+            #Check for last entry overflow
+            if len(mnist_data) <= imagen+batch_size:
+                batch = mnist_data[imagen:len(mnist_data)-1]
+                class_batch = classes[imagen:len(classes)-1]
 
             #Get the network's predictions
             result = mymodel.forward(batch)
@@ -69,6 +77,9 @@ def mnist():
             target = np.array(class_batch)
             #Find the accuracy
             accuracy = np.mean(predictions==target) * 100
+            #Add batch accuracy to epoch accuracy
+            accuracy_epoch += accuracy
+
 
             #Uncomment to see the amount of right values
             #strike = 0
@@ -82,11 +93,17 @@ def mnist():
 
             #Optimize network's parameters
             opt.forward()
-        #Print the last batch results in the epoch
-        print("Epoch: ", epoch, "\tAccuracy in the last batch: ", int(accuracy), "%\tLoss: ", mymodel.calculate_loss(batch, target))
+        #Print the information about an epoch pass
+        print("Epoch: {}\tAccuracy in the last batch: {:.6f} %\tAverage accuracy in epoch: {:.2f} %\tLast batch loss: {}".format(epoch, accuracy, accuracy_epoch/(len(mnist_data)/batch_size), mymodel.calculate_loss(batch, target)))
         #Accuracy may be 100% while loss is not 0.
         #This is due to the network being "uncertain"
-    
+
+    test_model_mnist(mymodel)
+
+    #Save the model
+    mymodel.save_model('params.npy')
+
+def test_model_mnist(model):
     #Open the mnist test csv file
     mnist_file = open(os.sys.path[0] + "\\mnist_test.csv")
     #Read each line as row into the mnist_data array
@@ -102,36 +119,24 @@ def mnist():
     classes = []
 
     #Go through each image in mnist_data
-    for image in range(len(mnist_data) - 1):
+    for image in range(len(mnist_data)):
         #Append new class value(see the mnist dataset structure)
         classes.append(int(mnist_data[image][0]))
-        #We want to have neural network's inputs in the range of 0.01 - 0.99
-        mnist_data[image] = ((np.asfarray(mnist_data[image].split(",")[1:]) / 255.0 * 0.99) + 0.01)
+        #We want to have neural network's inputs in the range of 0.00001 - 0.99999
+        mnist_data[image] = ((np.asfarray(mnist_data[image].split(",")[1:]) / 255.0 * 0.99999) + 0.00001)
 
-    #Create the test batch
-    test_batch = len(mnist_data) - 2
+    #Make a forward pass using all the numbers
+    test_result = model.forward(mnist_data)
+    #Convert the results to index form
+    test_result = np.argmax(test_result, axis=1)
+    #Find accuracy
+    accuracy = np.mean(test_result == np.array(classes)) * 100
 
-    #This is a raw solution. I will change the last bit later on
-    for imagen in range(0,(len(mnist_data)-1) - test_batch,test_batch):
-        #Get the arrays of images and classes
-        batch = mnist_data[imagen:imagen+test_batch]
-        class_batch = classes[imagen:imagen+test_batch]
+    #Print the testing results
+    print("Accuracy in the test batch: {:.6f} %\tLoss: {:.6f}".format(accuracy, model.calculate_loss(mnist_data, classes)))
 
-        #Get the network's predictions
-        result = mymodel.forward(batch)
-        #Get the most certain prediction
-        predictions = np.argmax(result, axis=1)
-        #Convert classes to numpy array
-        target = np.array(class_batch)
-        #Find the accuracy
-        accuracy = np.mean(predictions==target) * 100
-
-    print("Accuracy in the test batch: ", int(accuracy), "%\tLoss: ", mymodel.calculate_loss(batch, target))
     #Accuracy may be 100% while loss is not 0.
     #This is due to the network being "uncertain"
-
-    #Save the model
-    mymodel.save_model('params.npy')
 
 
 def test_loading_model():
@@ -140,9 +145,9 @@ def test_loading_model():
     mymodel = model.Model(lossfun.CrossEnthropy)
 
     #Create layers
-    dense1 = layer.Dense(784, 100, actfun.ReLu)
-    dense2 = layer.Dense(100, 50, actfun.ReLu)
-    dense3 = layer.Dense(50, 10, actfun.Softmax)
+    dense1 = layer.Dense(784, 15, actfun.ReLu)
+    dense2 = layer.Dense(15, 20, actfun.ReLu)
+    dense3 = layer.Dense(20, 10, actfun.Softmax)
 
     #Add layers to the model
     mymodel.addLayer(dense1)
@@ -152,49 +157,11 @@ def test_loading_model():
     #Load parameters to the model
     mymodel.load_model('params.npy')
 
-    #Open the mnist test csv file
-    mnist_file = open(os.sys.path[0] + "\\mnist_test.csv")
-    #Read each line as row into the mnist_data array
-    mnist_data = mnist_file.readlines()
-    #Close the mnist file
-    mnist_file.close()
-
-    #Split each line(row(string) in mnist data) by comma
-    for img in mnist_data:
-        img = np.array(img.split(","))
-
-    #Array of class values
-    classes = []
-
-    #Go through each image in mnist_data
-    for image in range(len(mnist_data) - 1):
-        #Append new class value(see the mnist dataset structure)
-        classes.append(int(mnist_data[image][0]))
-        #We want to have neural network's inputs in the range of 0.01 - 0.99
-        mnist_data[image] = ((np.asfarray(mnist_data[image].split(",")[1:]) / 255.0 * 0.99) + 0.01)
-
-    bsize = len(mnist_data) - 2
-
-    #This is a raw solution. I will change the last bit later on
-    for imagen in range(0,(len(mnist_data)-1) - bsize,bsize):
-        #Get the arrays of images and classes
-        batch = mnist_data[imagen:imagen+bsize]
-        class_batch = classes[imagen:imagen+bsize]
-
-        #Get the network's predictions
-        result = mymodel.forward(batch)
-        #Get the most certain prediction
-        predictions = np.argmax(result, axis=1)
-        #Convert classes to numpy array
-        target = np.array(class_batch)
-        #Find the accuracy
-        accuracy = np.mean(predictions==target) * 100
-
-    print("Accuracy in the test batch: ", int(accuracy), "%\tLoss: ", mymodel.calculate_loss(batch, target))
+    test_model_mnist(mymodel)
 
 
 def main():
-    mnist()
+    mnist_train()
     test_loading_model()
 
 
